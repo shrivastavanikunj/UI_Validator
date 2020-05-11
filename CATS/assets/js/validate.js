fad29327720f5e9d055e7ecaa6d353e62028ff14
shrivastavanikunj <65071406+shrivastavanikunj@users.noreply.github.com>
2020-05-12 00:09:20 +0530

    Update validate.js

--- CATS/assets/js/validate.js
+++ CATS/assets/js/validate.js
@@ -850,3 +850,168 @@ function CreateSlick(data, columns, options, tabid) {
     CreateGrid("myGrid" + tabid, data, columns, options, tabid);
 }
 
+
+
+/**
+ * convert row data to Hierarchy
+ * @param rows
+ * @returns {Array}
+ */
+function convert(rows) {
+    function exists(rows, parentId) {
+        for (var i = 0; i < rows.length; i++) {
+            if (rows[i].id === parentId) return true;
+        }
+        return false;
+    }
+
+    var nodes = [];
+    // get the top level nodes
+    for (var i = 0; i < rows.length; i++) {
+        var row = rows[i];
+        if (!exists(rows, row.parentId)) {
+            nodes.push({
+                id: row.id,
+                text: row.name,
+                iconCls: row.iconCls
+            });
+        }
+    }
+
+    var toDo = [];
+    for (var i = 0; i < nodes.length; i++) {
+        toDo.push(nodes[i]);
+    }
+    while (toDo.length) {
+        var node = toDo.shift(); // the parent node
+        // get the children nodes
+        for (var i = 0; i < rows.length; i++) {
+            var row = rows[i];
+            if (row.parentId === node.id) {
+                var child = {
+                    id: row.id,
+                    text: row.name,
+                    iconCls: row.iconCls
+                };
+                if (node.children) {
+                    node.children.push(child);
+                } else {
+                    node.children = [child];
+                }
+                toDo.push(child);
+            }
+        }
+    }
+    return nodes;
+}
+
+
+
+function amendcall(TestCaseId ,testcasename ) {
+    // alert(TestCaseId);
+    // var selectedtestfolder = jQuery('#tt').tree('getSelected').id;
+    // var selectedtestfoldername = jQuery('#tt').tree('getSelected').text;
+    jQuery("#overlay").show();
+    baseurl = jQuery('input[title="baseURL"]').val();
+    //var path = getPath(node.target);
+    // var action = "ADDFOLDER";
+
+    // alert(selectedtestfolder);
+    // // settestcaseId(72);
+    // TestCaseId = gettestcaseId();
+
+    var dataString = "urlreader=" +
+        "" + "&TestCaseId=" +  TestCaseId +
+        "&tabnumber=" + gettabid() +
+        "&rowto=0" +
+        "&rowfrom=0" ;
+    // alert(dataString);
+    $.ajax({
+        type: "POST",
+        url: baseurl + amendservleturl + "?" + dataString , //baseurl + TestManagementURL + "?" + addfolderString,
+        content: "text/plain",
+        success: function (CodeResponseText) {
+
+            var data = JSON.parse(CodeResponseText);
+            //
+            // var dataResponse = JSON.parse(CodeResponseText);
+            // var dataSize = dataResponse.length  - 1 ;
+            // var data = dataResponse[dataSize];
+
+            settestcaseId(data.testcaseId);
+            settabid(data.tabCount);
+            var AmendTestCaseId = data.testcaseId ;
+            var AmendTabCount = data.tabCount;
+
+            var itab = 1 ;
+
+
+                var dataString = "TestCaseId=" + AmendTestCaseId +
+                    "&tabnumber=" + itab ;
+
+
+                jQuery.getJSON(baseurl + renderurl ,  dataString,  function (data1) {
+
+                    // grid.setData(data);
+                    // grid.render();
+                    // attachAutoResizeDataGrid(grid, "myGrid" + tabID, "gridContainer");
+                    //
+                    grid = CreateSlick(data1, columns, options, data1[0].TAG_TAB);
+                    if ( AmendTabCount > 1 ) {
+                        createGridTab(AmendTestCaseId, getPath(), testcasename, getShwlink(), itab + 1, defaultenteredurl, AmendTabCount );
+                    }
+                    // grid.render();
+                    // attachAutoResizeDataGrid(grid, "myGrid" + tabID, "gridContainer");
+                });
+
+
+
+            //   alert ( baseurl + renderurl + "?" + dataString);
+            // default user entered url
+            jQuery("#overlay").hide();
+            // activatebuttons();
+            // setFileUploadHistory(PrjectName);
+            //add alert message on success
+            addAlert("alert alert-success", "Hooray!", "Test Case is generated, Click on Save button.");
+            activatebuttons();
+        },
+        error: function () {
+            jQuery('#teststatusFailure').text("* Something went wrong, try again!");
+        }
+    });
+
+}
+
+
+function createGridTab(AmendTestCaseId, getPath , testcasename , getShwlink , itab , defaultenteredurl , AmendTabCount) {
+
+    var dataString = 
+        "tabnumber=" + itab +
+        "&urlreader=" +
+        defaultenteredurl ;
+
+    settabid(itab);
+    jQuery('#tab-list').append(jQuery('<li id="#tab' + tabID + '"><a href="#tab' + tabID + '" role="tab" value = "'+tabID+'" name="myGrid' + tabID + '" data-toggle="tab">Tab ' + tabID + '<button class="close" type="button" title="Remove this' +
+        ' page">&times</button></a></li>'));
+    jQuery('#tab-content').append(jQuery('<div class="tab-pane fade" style="border: 0.5px solid #ddd" id="tab' + tabID + '"> <td valign="top" width="50%"> <div id="gridContainer">  <div id="myGrid' + tabID + '" style="width: 100%;' +
+        ' overflow:inherit; height: 300px;" class="my-grid"></div> </div> </td> </div>'));
+
+
+    jQuery.getJSON(baseurl + renderurl ,  dataString,  function (data1) {
+
+        // grid.setData(data);
+        // grid.render();
+        // attachAutoResizeDataGrid(grid, "myGrid" + tabID, "gridContainer");
+        //
+        var tabNum = data1[0].TAG_TAB
+         grid = CreateSlick(data1, columns, options, tabNum);
+        //CreateGrid("myGrid" + tabNum, data1, columns, options, tabNum );
+        if(itab < AmendTabCount)  {
+            createGridTab(AmendTestCaseId, getPath, testcasename, getShwlink, itab + 1, defaultenteredurl, AmendTabCount );
+        }
+        // grid.render();
+        // attachAutoResizeDataGrid(grid, "myGrid" + tabID, "gridContainer");
+    });
+}
+
+
